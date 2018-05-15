5b1f7b03b270c32c7115b894c770473dfed4b269
Omar <omar.amoako@yahoo.co.uk>
2018-05-15 17:36:50 +0100

    Initial Commit

---
+++ Marvel UniverseTests/Marvel_UniverseTests.swift
@@ -0,0 +1,36 @@
+//
+//  Marvel_UniverseTests.swift
+//  Marvel UniverseTests
+//
+//  Created by Omar  on 15/05/2018.
+//  Copyright © 2018 Omar. All rights reserved.
+//
+
+import XCTest
+@testable import Marvel_Universe
+
+class Marvel_UniverseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
